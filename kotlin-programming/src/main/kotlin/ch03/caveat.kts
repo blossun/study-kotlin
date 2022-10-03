0076203f7a4162db3b2f06bbec8ecd543167f619
blossun <blossunn@gmail.com>
2022-10-04 00:36:29 +0900

    [다재다능 코틀린 프로그래밍] ch03 - 함수를 사용하자

---
+++ kotlin-programming/src/main/kotlin/ch03/caveat.kts
@@ -0,0 +1,11 @@
+package ch03
+
+fun caveat1() = 2
+fun caveat2() = { 2 }
+fun caveat3(factor: Int) = { n: Int -> n * factor }
+
+println(caveat1())
+println(caveat2())
+println(caveat2()())
+println(caveat3(2))
+println(caveat3(2)(3))
