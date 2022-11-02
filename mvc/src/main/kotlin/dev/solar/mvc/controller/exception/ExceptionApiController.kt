576542fcdc37f82a6db4909efffb73837510bd36
blossun <blossunn@gmail.com>
2022-11-02 19:28:04 +0900

    [예외 처리] ControllerAdvice - 2. 특정 컨트롤러에서 예외를 한번에 잡아서 처리

--- mvc/src/main/kotlin/dev/solar/mvc/controller/exception/ExceptionApiController.kt
+++ mvc/src/main/kotlin/dev/solar/mvc/controller/exception/ExceptionApiController.kt
@@ -1,5 +1,8 @@
 package dev.solar.mvc.controller.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,10 +14,13 @@ class ExceptionApiController {
     @GetMapping("/hello")
     fun hello() {
         val list = mutableListOf<String>()
-
         val temp = list[0]
-//        if (true) {
-//            throw RuntimeException("강제 exception 발생")
-//생       }
+    }
+
+    // 클래스 내부에 있는 ExceptionHandler는 해당 컨트롤러 내에서 발생하는 Exception만 처리한다.
+    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
+    fun indexOutOfBoundsException(): ResponseEntity<String> {
+        println("controller exception handler")
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Index Error")
     }
 }
