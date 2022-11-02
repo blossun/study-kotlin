6395a0387604ff1a799bdd8946a46ed6cc6b0946
blossun <blossunn@gmail.com>
2022-11-02 21:12:00 +0900

    [예외 처리] ExceptionHandler를 활용하여 자세한 에러 정보 내려주기

--- mvc/src/main/kotlin/dev/solar/mvc/advice/GlobalControllerAdvice.kt
+++ mvc/src/main/kotlin/dev/solar/mvc/advice/GlobalControllerAdvice.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 // @ControllerAdvice : @Controller에서 글로벌하게 발생하는 Exception을 핸들링
 // Exception을 잡을 controller를 제한할 수도 있다.
 // ex) basePackages : 특정 패키지 경로에 있는 @RestController
-@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class])
+//@RestControllerAdvice(basePackageClasses = [ExceptionApiController::class]) //실습을 위해 글로벌 익셉션 핸들러 비활성화
 class GlobalControllerAdvice {
     @ExceptionHandler(value = [RuntimeException::class]) //처리할 Exception을 지정
     fun exception(e: RuntimeException): String {
