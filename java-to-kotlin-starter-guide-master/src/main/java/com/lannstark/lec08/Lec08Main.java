5eb1715bfec12d1e38efab02267c78821fd9ce0a
blossun <blossunn@gmail.com>
2022-11-06 16:33:15 +0900

    8강. 코틀린에서 함수를 다루는 방법

--- java-to-kotlin-starter-guide-master/src/main/java/com/lannstark/lec08/Lec08Main.java
+++ java-to-kotlin-starter-guide-master/src/main/java/com/lannstark/lec08/Lec08Main.java
@@ -2,14 +2,49 @@ package com.lannstark.lec08;
 
 public class Lec08Main {
 
-  public static void main(String[] args) {
-
-  }
-
-  public static void printAll(String... strings) {
-    for (String str : strings) {
-      System.out.println(str);
+    public static void main(String[] args) {
+        //4. 가변인자 활용할 때는
+        // (1) 배열을 직접 넣거나
+        // (2) comma를 이용해 여러 파라미터를 넣으면 된다.
+        final String[] array = { "A", "B", "C" };
+        printAll(array);
+        printAll("ha", "hi", "ho");
     }
-  }
+
+    //1. 함수 선언 문법
+    public int max(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        return b;
+    }
+
+    //2. default parameter
+    //자바는 OverLoading을 이용해서 기본값을 사용한다.
+    public static void repeatJava(String str, int num, boolean useNewLine) {
+        for (int i = 1; i < num; i++) {
+            if (useNewLine) {
+                System.out.println(str);
+            } else {
+                System.out.print(str);
+            }
+        }
+    }
+
+    public void repeatJava(String str, int num) {
+        repeatJava(str, num, true);
+    }
+
+    public void repeatJava(String str) {
+        repeatJava(str, 3, true);
+    }
+
+    //4. 같은 타입의 여러 파라미터 받기 (가변인자)
+    //문자열을 N개 받아 출력하는 예제
+    public static void printAll(String... strings) {
+        for (String str : strings) {
+            System.out.println(str);
+        }
+    }
 
 }
